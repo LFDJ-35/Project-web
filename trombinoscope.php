e2bf8becd36744b32010f902410eb5808603a83e
Spirito35 <spirito35220@gmail.com>
2026-03-07 12:29:52 +0100

    #14 WIP Trombinoscope

---
+++ trombinoscope.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="ie ie8" lang="fr"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><html lang="fr" class="no-js"><![endif]-->
+
+<head>
+    <title>Trombinoscope – Les membres de La Forge des Joueurs à Vitré</title>
+    <meta name="description"
+        content="Découvrez le trombinoscope de La Forge des Joueurs : les membres de l'association de jeux de rôle, jeux de société et figurines à Vitré." />
+    <?php require('importation-php/regles.php');
+    include('importation-php/telemetrie.php'); ?>
+</head>
+
+<body class="lfdj-maincontainer">
+
+    <?php require('importation-php/menu.php'); ?>
+
+    <div class="lfdj-bloc-text2">
+        <h2 style="text-align: center; color: var(--primary-color); grid-column:1/-1;">Membres de la Forge</h1>
+
+            <div class="trombi-container">
+                <div class="member-card">
+                    <div class="member-info">
+                         <img src="https://placehold.co/100x100/" alt="Image placeholder">
+                        <h1>Kalimsharr</h1>
+
+                        <hr>
+                        <div class="status">
+                            <span class="tag-president">Président</span> <span class="tag">Maître du Jeu</span> <span
+                                class="tag">Joueur</span>
+                        </div>
+
+                        <hr>
+
+                        <div class="games">
+                            <span class="tag">🐉 Donjons & Dragons</span> <span class="tag">🔨 Warhammer</span> <span
+                                class="tag">🧟 Zombicide</span>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+                  <div class="trombi-container">
+                <div class="member-card">
+                    <div class="member-info">
+                         <img src="https://placehold.co/100x100/" alt="Image placeholder">
+                        <h1>Spirito</h1>
+
+                        
+                        <hr>
+                        
+                        <div class="status">
+                            <span class="tag-president">Président</span> <span class="tag">Maître du Jeu</span> <span
+                                class="tag">Joueur</span>
+                        </div>
+
+                        <hr>
+
+                        <div class="games">
+                            <span class="tag">🐉 Donjons & Dragons</span> <span class="tag">🔨 Warhammer</span> <span
+                                class="tag">🧟 Zombicide</span>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+                        <div class="trombi-container">
+                <div class="member-card">
+                    <div class="member-info">
+                         <img src="https://placehold.co/100x100/" alt="Image placeholder">
+                        <h1>Lucaschou</h1>
+
+                        <hr>
+
+                        <div class="status">
+                            <span class="tag tag-president">Président</span> <span class="tag">Maître du Jeu</span> <span
+                                class="tag">Joueur</span>
+                        </div>
+
+                        <hr>
+
+                        <div class="games">
+                            <span class="tag">🐉 Donjons & Dragons</span> <span class="tag">🔨 Warhammer</span> <span
+                                class="tag">🧟 Zombicide</span>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+                        <div class="trombi-container">
+                <div class="member-card">
+                    <div class="member-info">
+                         <img src="https://placehold.co/100x100/" alt="Image placeholder">
+                        <h1>Terminator</h1>
+                        
+                        <hr>
+                        <div class="status">
+                            <span class="tag-president">Président</span> <span class="tag">Maître du Jeu</span> <span
+                                class="tag">Joueur</span>
+                        </div>
+
+                        <hr>
+
+                        <div class="games">
+                            <span class="tag">🐉 Donjons & Dragons</span> <span class="tag">🔨 Warhammer</span> <span
+                                class="tag">🧟 Zombicide</span>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    <?php require('importation-php/footer.php'); ?>
+
+</body>

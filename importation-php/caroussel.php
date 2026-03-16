1cdd0f2242dc889c65b71f51e609eeeed63de022
quentin-riffault <quentin.riffault@rakouns.bzh>
2026-03-16 20:55:31 +0100

    Récupère les photos avant de les afficher

--- importation-php/caroussel.php
+++ importation-php/caroussel.php
@@ -1,18 +1,25 @@
 <?php
-$photos = range(1, 54);
-shuffle($photos); // mélange déjà les images
-$photos = array_merge($photos, $photos);
+// On lit les différents fichiers présents dans images/photos-caroussel
+$all_images = glob("images/photos-caroussel/*.webp");
 
+if($all_images == False)
+{
+    // Une erreur s'est produite.
+    error_log("Impossible de récupérer les images du caroussel");
+}
+
+// mélange déjà les images
+shuffle($all_images);
 // Délai de départ aléatoire entre 0 et -90s (négatif = démarre "en cours de route")
 $delay = rand(0, 90);
 ?>
 
 <div class="lfdj-carousel-wrap">
     <div class="lfdj-carousel-track" style="animation-delay: -<?= $delay ?>s;">
-        <?php foreach ($photos as $n) : ?>
+        <?php foreach ($all_images as $photo) : ?>
             <div class="lfdj-carousel-item">
                 <img
-                    src="images/photos-caroussel/photo <?= $n ?>.webp"
+                    src="<?= $photo ?>"
                     alt="Photo de La Forge des Joueurs"
                     loading="lazy"
                 >
